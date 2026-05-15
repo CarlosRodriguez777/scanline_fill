324dabfd9b90f670a9f9af507bd5270fac7ff6e6
Carlos Andres Rodriguez Diaz <Est.carlos.rodrigu11@unimilitar.edu.co>
2026-05-15 10:33:08 -0500

    setup de html y clase ScanlineFill

---
+++ scanline.js
@@ -0,0 +1,12 @@
+/**
+ * Implementación del algoritmo Scanline Fill
+ */
+class ScanlineFill {
+    /**
+     * Constructor
+     * @param {CanvasRenderingContext2D} ctx Contexto 2D del canvas donde se dibujará.
+     */
+    constructor(ctx) {
+        this.ctx = ctx; // [cite: 69, 70]
+    }
+}
